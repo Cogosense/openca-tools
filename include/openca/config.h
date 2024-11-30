8595bf59f9dd154702b242db36a8eb2f539e67a3
Steve Williams <steve.williams@telxio.com>
2024-11-30 00:29:53 -0800

    Update To Build On Alpine v3.20

--- include/openca/config.h
+++ include/openca/config.h
@@ -1,29 +1,48 @@
 /* include/openca/config.h.  Generated from config.h.in by configure.  */
-/* include/openca/config.h.in.  Generated from configure.in by autoheader.  */
+/* include/openca/config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* enable full Tools debug messaging. */
 /* #undef DEBUG */
 
+/* Define to 1 if you have the 'bzero' function. */
+#define HAVE_BZERO 1
+
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
 
-/* Define to 1 if you have the <fcntl.h> header file. */
-/* #undef HAVE_FCNTL_H */
+/* Define to 1 if you have the 'fork' function. */
+#define HAVE_FORK 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
+/* Define to 1 if your system has a GNU libc compatible 'malloc' function, and
+   to 0 otherwise. */
+#define HAVE_MALLOC 1
+
+/* Define to 1 if you have the 'memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define to 1 if you have the 'setenv' function. */
+#define HAVE_SETENV 1
+
+/* Define to 1 if you have the 'socket' function. */
+#define HAVE_SOCKET 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdio.h> header file. */
-/* #undef HAVE_STDIO_H */
+#define HAVE_STDIO_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the 'strcasecmp' function. */
+#define HAVE_STRCASECMP 1
+
+/* Define to 1 if you have the 'strchr' function. */
+#define HAVE_STRCHR 1
 
 /* Define to 1 if you have the <strings.h> header file. */
 #define HAVE_STRINGS_H 1
@@ -31,17 +50,11 @@
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
 
-/* Define to 1 if you have the <sys/file.h> header file. */
-/* #undef HAVE_SYS_FILE_H */
+/* Define to 1 if you have the 'strrchr' function. */
+#define HAVE_STRRCHR 1
 
-/* Define to 1 if you have the <sys/ipc.h> header file. */
-/* #undef HAVE_SYS_IPC_H */
-
-/* Define to 1 if you have the <sys/param.h> header file. */
-/* #undef HAVE_SYS_PARAM_H */
-
-/* Define to 1 if you have the <sys/sem.h> header file. */
-/* #undef HAVE_SYS_SEM_H */
+/* Define to 1 if you have the 'strstr' function. */
+#define HAVE_STRSTR 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
@@ -52,6 +65,21 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
+/* Define to 1 if you have the 'vfork' function. */
+#define HAVE_VFORK 1
+
+/* Define to 1 if you have the <vfork.h> header file. */
+/* #undef HAVE_VFORK_H */
+
+/* Define to 1 if 'fork' works. */
+#define HAVE_WORKING_FORK 1
+
+/* Define to 1 if 'vfork' works. */
+#define HAVE_WORKING_VFORK 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
 /* Name of package */
 #define PACKAGE "openca-tools"
 
@@ -59,18 +87,23 @@
 #define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME ""
+#define PACKAGE_NAME "openca-tools"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING ""
+#define PACKAGE_STRING "openca-tools `cat VERSION`"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME ""
+#define PACKAGE_TARNAME "openca-tools"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION ""
+#define PACKAGE_VERSION "`cat VERSION`"
 
-/* Define to 1 if you have the ANSI C header files. */
+/* Define to 1 if all of the C89 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
 #define STDC_HEADERS 1
 
 /* BSD */
@@ -88,5 +121,20 @@
 /* Solaris */
 /* #undef TARGET_SOLARIS */
 
+/* Define to 1 if your <sys/time.h> declares 'struct tm'. */
+/* #undef TM_IN_SYS_TIME */
+
 /* Version number of package */
-#define VERSION "1.3.0"
+#define VERSION "`cat VERSION`"
+
+/* Define to empty if 'const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to rpl_malloc if the replacement function should be used. */
+/* #undef malloc */
+
+/* Define as a signed integer type capable of holding a process identifier. */
+/* #undef pid_t */
+
+/* Define as 'fork' if 'vfork' does not work. */
+/* #undef vfork */
